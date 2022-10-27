cd3ab0152049e764e2ec776704673bfd7eb300da
kimmi <kimmi@DESKTOP-IG7P6LO>
2022-10-27 10:21:02 +0900

    1주차 정리

---
+++ 1주차_정리(변수,데이터형,연산자)/src/MainClass.java
@@ -0,0 +1,17 @@
+import java.io.*;
+public class MainClass {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		FileReader fr=new FileReader("C:\\javaDev\news.txt");
+		int i=0;
+		StringBuffer sb=new StringBuffer();
+		while((i=fr.read())!=-1)
+		{
+			sb.append((char)i);
+		}
+		fr.close();
+		System.out.println(sb.toString());
+	}
+
+}
