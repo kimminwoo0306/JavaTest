029eeff582f117f9c7d33f9497613926b9ba0cec
kimminou0306 <kimmi@DESKTOP-IG7P6LO>
2022-11-29 09:21:06 +0900

    접근지정어(캡슐화)

---
+++ Chapter7_접근지정어(캡슐화)/src/com/sist/main/MainClass2.java
@@ -0,0 +1,15 @@
+package com.sist.main;
+
+public class MainClass2 {
+	final int a;
+	final static int b=200;;
+	MainClass2()
+	{
+		a=100;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
